a6ab713a8d3d36c9021304bc4aa985ab50c4e4aa
Ronelle Cudjoe <ronnycudjoe9@gmail.com>
2025-04-29 10:42:10 +0000

    updated credentials

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Cliq',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: const Color(0xFF4CAF50),
         scaffoldBackgroundColor: const Color(0xFF121212),
